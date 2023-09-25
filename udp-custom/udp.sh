7b7a3c09b3d15b1dc7fdb42ecda4fab2e42bbaf3
hokage <142867374+hokage2023@users.noreply.github.com>
2023-09-25 08:44:06 +0700

    Update udp.sh

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# Script UdpCustom 2023
-# Script By MARDHEX
-# https://t.me/mdxdev
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition 1.0
-# Auther  : givpn
-# Kuota Habisss
 # (C) Copyright 2023
 # =========================================
-# pewarna hidup
+
 BGreen='\e[1;32m'
 NC='\e[0m'
 cd
@@ -21,15 +13,12 @@ rm -rf log-install.txt
 rm -rf /usr/bin/usernew
 rm -rf /usr/bin/trial
 rm -rf /root/domain
-echo "\e[1;32m Update Menu.. \e[0m"
-sleep 1
-wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/usernew.sh
-wget -q -O /usr/bin/trial https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/trial.sh
+echo "\e[1;32m INSTALL SLOW DNS & UDP COSTUM... \e[0m"
 echo "\e[1;32m Proses Download Script Slowdns.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/hokagelegend2023/Lite/main/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
 echo "\e[1;32m Proses Download Script OpenVPN.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
+wget https://raw.githubusercontent.com/hokagelegend2023/Lite/main/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
 sleep 1
 chmod +x /usr/bin/usernew
 chmod +x /usr/bin/trial
@@ -50,7 +39,7 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Toll UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://raw.githubusercontent.com/hokagelegend2023/Lite/main/udp-custom/openvpn/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
 clear
 # install Config Default Udp
@@ -68,13 +57,13 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Default Config UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://raw.githubusercontent.com/hokagelegend2023/Lite/main/udp-custom/openvpn/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
 chmod 644 /root/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by MARDHEX
+Description=UDP Custom by HOKAGE LEGEND
 
 [Service]
 User=root
@@ -90,7 +79,7 @@ EOF
 else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by MARDHEX
+Description=UDP Custom by HOKAGE LEGEND
 
 [Service]
 User=root
